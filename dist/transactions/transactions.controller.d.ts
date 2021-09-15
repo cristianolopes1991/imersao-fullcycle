78ed0be46ac59f41861f0ddcb6802f5f25d3b23a
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2021-09-15 00:12:35 -0300

    Curso de full cycle e estudo de caso

---
+++ dist/transactions/transactions.controller.d.ts
@@ -0,0 +1,8 @@
+import { TransactionsService } from './transactions.service';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+export declare class TransactionsController {
+    private readonly transactionsService;
+    constructor(transactionsService: TransactionsService);
+    create(createTransactionDto: CreateTransactionDto): Promise<import("./entities/transaction.entity").Transaction>;
+    findAll(): Promise<import("./entities/transaction.entity").Transaction[]>;
+}
