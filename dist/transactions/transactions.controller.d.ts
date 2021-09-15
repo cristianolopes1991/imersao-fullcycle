e118b1042b715a30758cd8cba5b5ff2a2d233b99
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2021-09-15 00:18:58 -0300

    Ajustando git Ignore

--- dist/transactions/transactions.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { TransactionsService } from './transactions.service';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-export declare class TransactionsController {
-    private readonly transactionsService;
-    constructor(transactionsService: TransactionsService);
-    create(createTransactionDto: CreateTransactionDto): Promise<import("./entities/transaction.entity").Transaction>;
-    findAll(): Promise<import("./entities/transaction.entity").Transaction[]>;
-}
--- dist/transactions/transactions.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { Transaction } from './entities/transaction.entity';
-export declare class TransactionsService {
-    private transactionModel;
-    constructor(transactionModel: typeof Transaction);
-    create(createTransactionDto: CreateTransactionDto): Promise<Transaction>;
-    findAll(): Promise<Transaction[]>;
-}
